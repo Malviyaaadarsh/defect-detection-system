19148521109bc44ba68955e2af5c640f086e638c
Aadarsh Malviya <165465476+Malviyaaadarsh@users.noreply.github.com>
2025-06-10 13:50:30 +0000

    updating app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
 st.title("Defect Detection System")
+st.write("This model classifies images containing defects into Weld Defects , Paint Defects and Machinery Ddefects with various Labels. ")
 st.caption("### [This model is still developed by Aari Labs]  [Stay Tuned!]")
 
